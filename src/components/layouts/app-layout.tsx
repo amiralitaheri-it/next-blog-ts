9c574b7d2bea5d7eec6b4a21d6fe6d5fce5d76e1
Amir Ali Taheri <amiralitaheri2000th@gmail.com>
2022-08-11 18:04:09 +0430

    fix footer grow

--- src/components/layouts/app-layout.tsx
+++ src/components/layouts/app-layout.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 const AppLayout: React.FC<Props> = ({children}) => {
     return (
-        <>
+        <div className="flex flex-col h-screen">
             <Header/>
-            <div className='dark:text-gray-400 text-gray-700 grid place-items-center'>
+            <div className='dark:text-gray-400 text-gray-700 grid place-items-center grow'>
                 {children}
             </div>
             <Footer/>
-        </>
+        </div>
     );
 }
 
