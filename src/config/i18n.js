f43f835b9dc762e3d43beacc937cba81854cbe6a
Amir Ali Taheri <amiralitaheri2000th@gmail.com>
2022-08-16 02:04:56 +0430

    add comment to change config file

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -3,6 +3,7 @@ import {initReactI18next} from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
+// change it to nextjs one
 i18n
     .use(Backend)
     .use(LanguageDetector)
